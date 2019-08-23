3954af025421ed564a50b9e458f5e397156050d3
lxs24sxl <13539748633@139.com>
2019-08-23 09:46:13 +0800

    路由匹配

--- src/pages/timeline/index.js
+++ src/pages/timeline/index.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import './index.less';
 import TagManage from 'components/tag-manage';
-
 import { useParams } from 'hooks';
+import { Switch, Route, Link } from 'react-router-dom';
 
 function TimeLine(props) {
   const params = useParams();
+
   return (
     <div className="timeline">
+      <Link to={`/timeline/${params.type}/test`}>{params.type}</Link>
+
       <TagManage></TagManage>
-      timeline-{ params.type }
-  </div>
+
+      <Switch>
+        <Route path="/timeline/*/*" render={() => {
+          return (
+            <div>
+              三级路径 - {params.type}
+              <Link to={`/timeline/${params.type}`}>/timeline/android</Link>
+            </div>
+          )
+        }}></Route>
+
+        <Route path="/timeline/*" render={() => {
+          return (
+            <div>
+              timeline - {params.type}
+            </div>
+          )
+        }}></Route>
+      </Switch >
+    </div >
   )
 }
 
