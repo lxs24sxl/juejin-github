17348d6f4640e75bcd571fd22d6165feedab91ee
lxs24sxl <13539748633@139.com>
2019-08-28 14:31:02 +0800

    fix: 刷新菜单初始化

--- src/pages/timeline/components/list-header/list-nav/index.js
+++ src/pages/timeline/components/list-header/list-nav/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Divider } from 'antd';
 import classNames from 'classnames';
 import { UL, Nav, LI } from './style';
 import VoLink from 'components/vo-link';
-import { useStore, useDispatch, useSelector, connect } from 'react-redux'
-
+import {
+  // useStore, 
+  useDispatch,
+  useSelector,
+  // connect 
+} from 'react-redux'
+import { useLocation, useMount, useQuery } from 'hooks';
 
 function ListNav(props) {
   // const store = useStore();
   const dispatch = useDispatch();
-  
 
+  // 获取query参数
+  const { sort } = useQuery();
+
+  // 排序
+  const { navigate, location } = useLocation();
+
+  // 全局属性
   const oSort = useSelector(state => state.timeline.sort);
+
+  /**
+   * 跳转路径
+   * @param {String} sort 排序方式
+   */
   const changeSort = (sort) => {
 
+    // 派发redux
     dispatch({
       type: 'SORT_UPDATE',
       data: {
@@ -21,31 +38,57 @@ function ListNav(props) {
       }
     })
 
-    // console.log('sort', sort, oSort);
+    // 改变路径
+    navigate(`${location.pathname}?sort=${sort}`, { replace: true });
 
-    // console.log('store', store, store.getState(), dispatch)
   }
+
+  // 类名
   const activeClassName = (sort) => {
     return classNames({
       'active': oSort === sort,
     });
   };
-  
+
+  // 菜单列表
+  const sortList = [
+    { sort: 'popular', title: '热门' },
+    { sort: 'newest', title: '最新' },
+    { sort: 'three_days_hottest', title: '热榜' }
+  ];
+
+  // componentWillMounte事件
+  useMount(() => {
+    dispatch({
+      type: 'SORT_UPDATE',
+      data: {
+        sort: sort || 'popular'
+      }
+    })
+  })
+
   return (
     <Nav>
       <UL>
-        <LI onClick={() => changeSort('popular')}>
-          {/* {oSort} */}
-          <VoLink className={activeClassName('popular')}>热门</VoLink>
-        </LI>
-        <Divider type="vertical" />
-        <LI onClick={() => changeSort('newest')}>
-          <VoLink  className={activeClassName('newest')}>最新</VoLink>
-        </LI>
-        <Divider type="vertical" />
-        <LI onClick={() => changeSort('three_days_hottest')}>
-          <VoLink  className={activeClassName('three_days_hottest')}>热榜</VoLink>
-        </LI>
+        {
+          sortList.map((item, index) => {
+            return (
+              <Fragment key={item.sort}>
+                {
+                  index === 0
+                    ? null
+                    : <Divider type="vertical" />
+                }
+
+                <LI onClick={() => changeSort(item.sort)}>
+                  <VoLink className={activeClassName(item.sort)}>
+                    {item.title}
+                  </VoLink>
+                </LI>
+              </Fragment>
+            )
+          })
+        }
       </UL>
     </Nav>
   )
